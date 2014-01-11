ed7d5c31131018e214f16c3fb59d0252baa51529
bluezeal <bluezeal@gmail.com>
2014-01-11 15:51:21 +0530

    update ccavenue.rb
    update ccavenue.rb

--- ccavenue.rb
+++ ccavenue.rb
@@ -1,24 +1,22 @@
-#require File.dirname(__FILE__) + '/active_merchant_ccavenue/helper.rb'
-#require File.dirname(__FILE__) + '/active_merchant_ccavenue/return.rb'
-
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
     module Integrations #:nodoc:
       module Ccavenue
-		autoload :Return, File.dirname(__FILE__) + '/ccavenue/return.rb'
-        autoload :Helper, File.dirname(__FILE__) + '/ccavenue/helper.rb'
-              
-		  
-        mattr_accessor :merchant_id
-        mattr_accessor :working_key        
-        mattr_accessor :service_url 
-        self.service_url = 'https://www.ccavenue.com/shopzone/cc_details.jsp' 		
-	
-
-        def self.setup
-			yield(self)
-		end
-
+		 
+        autoload :Notification, File.dirname(__FILE__) + '/ccavenue/notification.rb'
+        autoload :Helper, File.dirname(__FILE__) + '/ccavenue/helper.rb' 
+        	
+        mattr_accessor :service_url 		
+        self.service_url = 'https://www.ccavenue.com/shopzone/cc_details.jsp' 
+		
+		def self.helper(order, account, options = {})
+          Helper.new(order, account, options)
+        end
+		
+		def self.notification(post,options={})
+		    Notification.new(post,options)
+        end
+		
         def self.return(query_string, options = {})
           Return.new(query_string)
         end
@@ -27,3 +25,4 @@ module ActiveMerchant #:nodoc:
     end
   end
 end
+
